08c540a4779ae52920a57978789660d9b9406baf
jinsang <jinsang@nhndata.com>
2022-08-30 18:31:47 +0900

    faker를 이용한 더미 제품 목록, 유저 목록 API 생성

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const { faker } = require('@faker-js/faker')
 const app = express()
 const cors = require('cors')
 app.use(cors())
@@ -10,7 +11,38 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, './view-with-react/build/index.html'))
 })
 
-app.get('/product', function (req, res) {
+const Users = []
+const Products = []
+
+function createRandomProduct() {
+  return {
+    productId: faker.datatype.uuid(),
+    productName: faker.commerce.productName(),
+    productDescription: faker.commerce.productDescription(),
+    sellerName: faker.internet.userName(),
+    sellerEmail: faker.internet.email(),
+    company: faker.company.name(),
+    companyDomain: faker.internet.url(),
+    productImg: faker.image.business(200, 200, true),
+    price: faker.commerce.price(100, 200, 0),
+    registeredAt: faker.date.past()
+  }
+}
+
+function createRandomUser() {
+  return {
+    userId: faker.datatype.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+    password: faker.internet.password(),
+    birthdate: faker.date.birthdate(),
+    registeredAt: faker.date.past(),
+    addressCity: faker.address.cityName()
+  }
+}
+
+app.get('/products', function (req, res) {
   console.log('===== GET call')
   console.log('***** req.headers: >>' + JSON.stringify(req.headers) + '<<')
 
@@ -18,7 +50,28 @@ app.get('/product', function (req, res) {
   console.log('***** req.query: ' + JSON.stringify(req.query))
   console.log('***** req.body: ' + JSON.stringify(req.body))
 
-  res.json({ name: 'black shoes' })
+  Products.length = 0
+  Array.from({ length: 10 }).forEach(() => {
+    Products.push(createRandomProduct())
+  })
+
+  res.json(Products)
+})
+
+app.get('/users', function (req, res) {
+  console.log('===== GET call')
+  console.log('***** req.headers: >>' + JSON.stringify(req.headers) + '<<')
+
+  console.log('***** req.url: ' + JSON.stringify(req.url))
+  console.log('***** req.query: ' + JSON.stringify(req.query))
+  console.log('***** req.body: ' + JSON.stringify(req.body))
+
+  Users.length = 0
+  Array.from({ length: 10 }).forEach(() => {
+    Users.push(createRandomUser())
+  })
+
+  res.json(Users)
 })
 
 app.get('*', function (req, res) {
