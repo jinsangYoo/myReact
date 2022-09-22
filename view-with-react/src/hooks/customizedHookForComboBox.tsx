f10482287a994387baf47f4350fb16f0eadd1c45
jinsang <jinsang@nhndata.com>
2022-09-22 13:55:45 +0900

    제품 옵션 초기값 설정

--- view-with-react/src/hooks/customizedHookForComboBox.tsx
+++ view-with-react/src/hooks/customizedHookForComboBox.tsx
@@ -52,6 +52,10 @@ export default function CustomizedHook({
   const [value, setValue] = React.useState<string | null>(samples[defaultValueIndex])
   const [inputValue, setInputValue] = React.useState('')
 
+  React.useEffect(() => {
+    onSelectedOptions(samples[defaultValueIndex])
+  }, [])
+
   return (
     <div>
       <Autocomplete
