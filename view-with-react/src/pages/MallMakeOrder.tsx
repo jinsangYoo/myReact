a02d48f07edcc268b4f621a21a375266af7c1b3d
jinsang <jinsang@nhndata.com>
2022-09-22 18:05:18 +0900

    주문서 작성 화면 작업중

--- view-with-react/src/pages/MallMakeOrder.tsx
+++ view-with-react/src/pages/MallMakeOrder.tsx
@@ -1,4 +1,14 @@
-import React from 'react'
+import React, { useState, useReducer, useEffect, useCallback, useMemo } from 'react'
+import { styled } from '@mui/material/styles'
+import TextField from '@mui/material/TextField'
+import { Button } from '@mui/material'
+
+import { ProductForType, useCart, useOrder } from '../hooks'
+
+const Image = styled('img')({
+  width: '100%',
+  borderRadius: 10
+})
 
 export default function MallMakeOrder() {
   return <p>몰 주문서 작성 입니다.</p>
