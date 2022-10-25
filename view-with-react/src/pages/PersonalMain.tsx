961760adc2acae726206b9b01e1b5279a7756463
jinsang <jinsang@nhndata.com>
2022-10-25 17:02:00 +0900

    personmal main 에 pl 적용

--- view-with-react/src/pages/PersonalMain.tsx
+++ view-with-react/src/pages/PersonalMain.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
-import { Button } from '@jinsang/slimer-react'
+import React, { useLayoutEffect } from 'react'
+import {
+  AceConfiguration,
+  ACParams,
+  ACS,
+  ACEResponseToCaller,
+  ACProduct,
+  ACEGender,
+  ACEMaritalStatus,
+  Button
+} from '@jinsang/slimer-react'
+import { sendCommonWithPromise, sendCommonWithCB, getRandomIntInclusive } from '../utils'
 
+const title = '대문_main'
+const randomValueForScreen = getRandomIntInclusive(0, 999).toString()
 export default function PersonalMain() {
+  useLayoutEffect(() => {
+    const msg = `>>${title}<< >>${randomValueForScreen}<<`
+    const params = ACParams.init(ACParams.TYPE.EVENT, msg)
+    sendCommonWithPromise(msg, params)
+  }, [])
+
   return (
     <div>
       <p>대문 메인 입니다.</p>
-      <Button label="됐다!!" />
+      <Button label="사이트 첫화면!!" />
     </div>
   )
 }
