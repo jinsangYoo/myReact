454fa21bf9eb65fe0e2cad87df37d5e25a7d26bb
jinsang <jinsang@nhndata.com>
2023-10-10 18:27:43 +0900

    iframe 방법 수집중

--- view-with-react/src/pages/PersonalAbout.tsx
+++ view-with-react/src/pages/PersonalAbout.tsx
@@ -9,6 +9,7 @@ import {
   ACEMaritalStatus
 } from '@jinsang/slimer-react'
 import { sendCommonWithPromise, sendCommonWithCB, getRandomIntInclusive } from '../utils'
+import { Button } from '@mui/material'
 
 const title = '대문_about'
 const randomValueForScreen = getRandomIntInclusive(0, 999).toString()
@@ -19,5 +20,40 @@ export default function PersonalAbout() {
     sendCommonWithPromise(msg, params)
   }, [])
 
-  return <p>대문 여긴어디 입니다.</p>
+  const iframeRef = React.useRef(null)
+  function sendToIFrame() {}
+
+  return (
+    <>
+      <p>대문 여긴어디 입니다.</p>
+      <div>
+        <ul>
+          <li>
+            <iframe
+              title="대문 여긴어디 입니다."
+              src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d4806.420002661456!2d-89.87919774410639!3d31.58858477434875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e6!4m0!4m3!3m2!1d31.5883935!2d-89.8794326!5e0!3m2!1sen!2sus!4v1581760461224!5m2!1sen!2sus"
+              width="1140"
+              height="410"
+              style={{ border: '0' }}
+            />
+          </li>
+          <li>
+            <Button variant="outlined" onClick={sendToIFrame}>
+              send to IFrame.
+            </Button>
+          </li>
+          <li>
+            <iframe
+              id="cardGame"
+              title="cardGame"
+              width="500"
+              height="200"
+              style={{ border: '0' }}
+              src="http://localhost:52274/"
+            />
+          </li>
+        </ul>
+      </div>
+    </>
+  )
 }
