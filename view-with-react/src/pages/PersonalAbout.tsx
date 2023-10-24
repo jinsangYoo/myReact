b134074b064fa9df442b5205e21f7d0148c9147b
jinsang <jinsang@nhndata.com>
2023-10-24 14:32:10 +0900

    iframe 과 기본적인 통신 확인 버전

--- view-with-react/src/pages/PersonalAbout.tsx
+++ view-with-react/src/pages/PersonalAbout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect } from 'react'
+import React, { useEffect, useLayoutEffect, useCallback } from 'react'
 import {
   AceConfiguration,
   ACParams,
@@ -14,12 +14,13 @@ import { Button } from '@mui/material'
 const title = '대문_about'
 const randomValueForScreen = getRandomIntInclusive(0, 999).toString()
 export default function PersonalAbout() {
+  const iframeRef = React.useRef<HTMLIFrameElement>(null)
   useEffect(() => {
-    // TODO
-    // SDK 와 함께 listener 정리가 필요.
-    // return () => {
-    //   window.top?.removeEventListener('didMounted')
-    // }
+    window.addEventListener('message', ACS.handleMessage)
+    return () => {
+      window.removeEventListener('message', ACS.handleMessage)
+      ACS.removeAllIfreameRefs()
+    }
   }, [])
 
   useLayoutEffect(() => {
@@ -28,18 +29,30 @@ export default function PersonalAbout() {
     sendCommonWithPromise(msg, params)
   }, [])
 
-  const iframeRef = React.useRef(null)
-  function sendToIframe() {
+  const sendToIframe = () => {
+    // window.postMessage(
+    //   {
+    //     type: 'didMounted1',
+    //     message: '젭알!!!'
+    //   },
+    //   '*'
+    // )
     console.log('in sendToIframe')
-    window.postMessage(
-      JSON.stringify({
-        type: 'type:sendToIframe',
-        datas: {
-          aa: 'datas:aa:sendToIframe'
-        }
-      }),
-      'http://localhost:3000'
-    )
+    ACS.printIfreameRefs()
+  }
+
+  const handleLoad = () => {
+    console.log('Ready for iframeRef.')
+    ACS.addIframeRef(iframeRef, 'http://localhost:52274/')
+    // setTimeout(() => {
+    //   if (!iframeRef.current) {
+    //     return
+    //   }
+    //   iframeRef.current.contentWindow?.postMessage(
+    //     'initInIframe',
+    //     'http://localhost:52274/'
+    //   )
+    // }, 100)
   }
 
   return (
@@ -47,7 +60,7 @@ export default function PersonalAbout() {
       <p>대문 여긴어디 입니다.</p>
       <div>
         <ul>
-          <li>
+          {/* <li>
             <iframe
               title="대문 여긴어디 입니다."
               src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d4806.420002661456!2d-89.87919774410639!3d31.58858477434875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e6!4m0!4m3!3m2!1d31.5883935!2d-89.8794326!5e0!3m2!1sen!2sus!4v1581760461224!5m2!1sen!2sus"
@@ -55,7 +68,7 @@ export default function PersonalAbout() {
               height="410"
               style={{ border: '0' }}
             />
-          </li>
+          </li> */}
           <li>
             <Button variant="outlined" onClick={sendToIframe}>
               send to Iframe.
@@ -70,6 +83,7 @@ export default function PersonalAbout() {
               height="200"
               style={{ border: '0' }}
               src="http://localhost:52274/"
+              onLoad={handleLoad}
             />
           </li>
         </ul>
